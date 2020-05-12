fd802287304d970194c1916543825243280f2680
harsha3004 <63159546+harsha3004@users.noreply.github.com>
2020-05-12 15:19:11 +0530

    Add files via upload

---
+++ add.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+int x,y;
+printf("enter first number:");
+scanf("%d",&x);
+printf("enter second number:");
+scanf("%d",&y);
+int add=x+y;
+int sub=x-y;
+int mul=x*y;
+int div=x/y;
+printf("sum of  %d + %d = %d\n",x,y,add );
+printf("difference of %d - %d = %d\n",x,y,sub);
+printf("multiplication of %d * %d = %d\n",x,y,mul);
+printf("difference of %d / %d = %d\n",x,y,div);
+return 0;
+}
