a3410dccdb8fd82d132ff6156ca95663aa398798
harsha3004 <63159546+harsha3004@users.noreply.github.com>
2020-05-12 15:45:07 +0530

    Add files via upload

---
+++ add3.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	int x,y,z,add;
+	printf("enter three numbers:");
+	scanf("%d%d%d",&x,&y,&z);
+	add=x+y+z;
+	printf("sum of numbers %d+%d+%d = %d",x,y,z,add);
+	return 0;
+}
